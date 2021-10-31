141aa1176f2eb24f0feb9880935a57660feff05d
premchand228 <pream.chand228@gmail.com>
2021-10-31 13:33:21 +0530

    pushing the stage01 changes

--- src/stage_01_prepare_data.py
+++ src/stage_01_prepare_data.py
@@ -4,6 +4,7 @@ import shutil
 from tqdm import tqdm
 import logging
 from src.utils.common import read_yaml,create_directory
+import random
 
 
 
@@ -24,6 +25,29 @@ def main(config_path,params_path):
     input_data=os.path.join(source_data_dir,source_data_file)
     split=params["prepare"]["split"]
     seed=params["prepare"]["seed"]
+    random.seed(seed)
+    artifacts=config["artifacts"]
+    artifacts_dir=artifacts["ARTIFACTS_DIR"]
+    prepare_data_dir=os.path.join(artifacts_dir,  artifacts["PREPARED_DATA"])
+
+    create_directory([prepare_data_dir])
+
+    train_data_path= os.path.join(prepare_data_dir,artifacts["TRAIN_DATA"])
+    test_data_path= os.path.join(prepare_data_dir,artifacts["TEST_DATA"])
+    logging.info(train_data_path)
+    print(train_data_path)
+    logging.info(test_data_path)
+    encode="utf8"
+    with open(input_data,encoding=encode) as fd_in:
+        with open(train_data_path,"w",encoding=encode) as fd_out_train:
+            with open(test_data_path,"w",encoding=encode) as fd_out_test:
+                pass
+                #process_posts(fd_in,fd_out_train,fd_out_test,"<python>",split)
+
+
+
+
+
 
 
 
@@ -32,13 +56,13 @@ def main(config_path,params_path):
 if __name__ == '__main__':
     args = argparse.ArgumentParser()
     args.add_argument("--config", "-c", default="configs/config.yaml")
-    args.add_argument("--params", "-c", default="params.yaml")
+    args.add_argument("--params", "-p", default="params.yaml")
     parsed_args = args.parse_args()
 
     try:
         logging.info("\n********************")
         logging.info(f">>>>> stage {STAGE} started <<<<<")
-        main(config_path=parsed_args.config)
+        main(config_path=parsed_args.config,params_path=parsed_args.params)
         logging.info(f">>>>> stage {STAGE} completed! all the data are saved in local <<<<<n")
     except Exception as e:
         logging.exception(e)
