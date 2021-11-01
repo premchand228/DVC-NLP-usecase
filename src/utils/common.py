4dbb27c534f79e2974fc0c01167a8de12c197def
premchand228 <pream.chand228@gmail.com>
2021-11-01 15:24:27 +0530

    pushing evaluate.py changes

--- src/utils/common.py
+++ src/utils/common.py
@@ -29,6 +29,11 @@ def get_df(path_to_data: str, sep: str="\t") -> pd.DataFrame:
     logging.info(f"The input data frame {path_to_data} size is {df.shape}\n")
     return df
 
+def save_json(path, data):
+    with open(path, "w") as f:
+        json.dump(data, f, indent=4)
+
+    logging.info(f"json file saved at: {path}")
 
 
 def save_reports(report: dict, report_path: str, indentation=4):
